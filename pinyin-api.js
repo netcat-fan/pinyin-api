691d2201e3d487edf2d41052d1eb89d62c3976d9
root <root@centos7.5>
2025-06-12 16:10:28 +0800

    change / to *

--- pinyin-api.js
+++ pinyin-api.js
@@ -4,7 +4,7 @@ const serverless = require('serverless-http');
 const app = express();
 app.use(express.json());
 
-app.post('/', (req, res) => {
+app.post('*', (req, res) => {
   const { text } = req.body || {};
   if (!text) {
     res.status(400).json({ error: 'Missing text' });
