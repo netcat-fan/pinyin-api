6198b3e255984be40e8d6dc988caa6e401b65d64
root <root@centos7.5>
2025-06-12 16:25:55 +0800

    cancel path *

--- pinyin-api.js
+++ pinyin-api.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const pinyin = require('pinyin');
 const serverless = require('serverless-http');
+const pinyin = require('pinyin');
 const app = express();
 app.use(express.json());
 
-app.post('*', (req, res) => {
+app.post('/', (req, res) => {
   const { text } = req.body || {};
   if (!text) {
     res.status(400).json({ error: 'Missing text' });
@@ -12,10 +12,6 @@ app.post('*', (req, res) => {
   }
   const result = pinyin(text, { style: pinyin.STYLE_TONE2 }).flat().join(' ');
   res.status(200).json({ pinyin: result });
-});
-
-app.all('*', (req, res) => {
-  res.status(405).json({ error: 'Method Not Allowed' });
 });
 
 module.exports.handler = serverless(app);
